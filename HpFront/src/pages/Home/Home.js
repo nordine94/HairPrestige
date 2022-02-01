749419e4552e1e764c9b8a3e1d49556246ec4875
Khoudou Nordine <nordine-khoudou@hotmail.fr>
2022-02-01 16:26:25 +0100

    bandeau dans home

--- HpFront/src/pages/Home/Home.js
+++ HpFront/src/pages/Home/Home.js
@@ -1,11 +1,13 @@
 import AccueilServices from '../../components/AccueilServices/AccueilServices';
+import Bandeau from '../../components/Bandeau/Bandeau';
 import './Home.css';
 
 const Home = () => {
-    return (  
-        <div className="home">
-            <AccueilServices />
-        </div>
+    return (
+      <div className="home">
+        <Bandeau />
+        <AccueilServices />
+      </div>
     );
 }
  
