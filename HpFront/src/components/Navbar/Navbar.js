58e685aed4b7ba7694bea0c7b6b254b8695889d8
ImNotAOwl <siegel.matthieu@ymail.com>
2022-02-01 09:53:38 +0100

    :white_check_mark: web + :white_check_mark: responsive

--- HpFront/src/components/Navbar/Navbar.js
+++ HpFront/src/components/Navbar/Navbar.js
@@ -5,38 +5,40 @@ import logo from "../../assets/img/logo_hairPrestige.png";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const navLinks = [
+    { "text": "Accueil","chemin": "/"},
+    { "text": "Conseils", "chemin": "/conseils"},
+    { "text": "Contact", "chemin": "/contact"}
+  ];
 
-    const hamburger = useRef();
-    const navMenu = useRef();
+  const hamburger = useRef();
+  const navMenu = useRef();
 
-    const mobileMenu = () => {
-        hamburger.current.classList.toggle("active");
-        navMenu.current.classList.toggle("active");
-    }
+  const mobileMenu = () => {
+    hamburger.current.classList.toggle("active");
+    navMenu.current.classList.toggle("active");
+  };
 
-    
-
-
+  const closeMenu = () => {
+    hamburger.current.classList.remove("active");
+    navMenu.current.classList.remove("active");
+  }
 
   return (
     <nav className="navbar">
-      <NavLink to="/" className="logo"><img src={logo} alt="Logo Hair Prestige"/></NavLink>
+      <NavLink to="/" className="logo">
+        <img src={logo} alt="Logo Hair Prestige" />
+      </NavLink>
       <ul className="nav_menu" id="menu_navbar" ref={navMenu}>
-        <li className="nav_item">
-          <NavLink className="nav_link" to="/">
-            Accueil
-          </NavLink>
-        </li>
-        <li className="nav_item">
-          <NavLink className="nav_link" to="/conseils">
-            Conseils
-          </NavLink>
-        </li>
-        <li className="nav_item">
-          <NavLink className="nav_link" to="/contact">
-            Contact
-          </NavLink>
-        </li>
+        {navLinks.map((navLink, index) => {
+          return (
+            <li className="nav_item" key={`link-${index}`}>
+              <NavLink className="nav_link" to={`${navLink.chemin}`} onClick={closeMenu}>
+                {`${navLink.text}`}
+              </NavLink>
+            </li>
+          );
+        })}        
       </ul>
       <div className="hamburger" ref={hamburger} onClick={mobileMenu}>
         <span className="bar"></span>
