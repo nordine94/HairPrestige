19c77cd5c3479dea4da6a604d86b901a78caec96
ImNotAOwl <siegel.matthieu@ymail.com>
2022-02-01 09:54:59 +0100

    add Conseils route

--- HpFront/src/App.js
+++ HpFront/src/App.js
@@ -1,8 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Bandeau from './components/Bandeau/Bandeau';
 import Home from './pages/Home/Home';
+import Conseils from './pages/Conseils/Conseils';
 
 import './App.css';
+
 
 
 function App() {
@@ -12,6 +14,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home/>}/>
+          <Route path="/conseils" element={<Conseils/>}/>
         </Routes>
       </Router>
       <Bandeau/>
