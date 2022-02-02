78f98c196ac40eba8a2f4fc5f758525107cd1e10
Lem0nRavioli <hirona_mj@hotmail.fr>
2022-02-02 21:10:33 +0100

    prototype cartes accueil, responsive ok

--- HpFront/src/components/PhotoTextLR/PhotoTextLR.js
+++ HpFront/src/components/PhotoTextLR/PhotoTextLR.js
@@ -1,25 +1,27 @@
 import './PhotoTextLR.css';
 
 const PhotoTextLR = ({ props }) => {
+    if (props.imgRight) {
+        return (
+            <div className="phototextlr">
+                <div class="text">
+                    {
+                        <div className="paragraphe">{props.content}</div>
+                    }
+                </div>
+                <img className="photo" src={props.image} alt={props.alt} />
+            </div>
+        );
+    }
+
+
     return (
         <div className="phototextlr">
             <img className="photo" src={props.image} alt={props.alt} />
-            <div class="text">
-                <div class="paragraphe">
-                    La société C.F.R - <b>Centre de Formation et Recrutement</b> a été créée en <b> 2016</b> et
-                    détient l’autorisation préfectorale pour exercer dans le milieu de la formation
-                    professionnelle.
-                </div>
-                {/* <p class="par1"> CFR est <b> un centre de
-                    formation pluridisciplinaire </b> qui a pour mission de former l’ensemble des profils
-                    désirant se former à l’un de nos domaines d’expertise.
-                </p>
-                <p class="par1">
-                    Nous nous adressons autant aux demandeurs d’emploi qu’aux entreprises souhaitant former
-                    leur personnel ou tout autre personne souhaitant se former. CFR s’appuie sur une solide
-                    expérience
-                    dans les métiers de l’informatique et de la finance.
-                </p> */}
+            <div className="text">
+                {
+                    <div className="paragraphe">{props.content}</div>
+                }
             </div>
         </div>
     );
--- HpFront/src/pages/Home/Home.js
+++ HpFront/src/pages/Home/Home.js
@@ -4,17 +4,51 @@ import FeedbackBox from '../../components/FeedbackBox/FeedbackBox';
 import './Home.css';
 
 import PhotoTextLR from '../../components/PhotoTextLR/PhotoTextLR';
-import image from '../../assets/img/bride.jpg';
+import image_1 from '../../assets/img/bride.jpg';
+import image_2 from '../../assets/img/ciseaux.jpg';
+import cards_data from './home_cards_data';
 
 const Home = () => {
-    return (
-      <div className="home">
-        <Bandeau />
-        {/* <AccueilServices /> */}
-        <PhotoTextLR  props={{"image": image, "alt":"image of a bride"}} />
-        <FeedbackBox />
-      </div>
-    );
+  // const cards_data = [
+  //   {
+  //     "key": 1111111,
+  //     "imgRight": false,
+  //     "image": image_1,
+  //     "alt": "image of a bride",
+  //     "content": "La société C.F.R - Centre de Formation et Recrutement a été créée en  2016 et détient l’autorisation préfectorale pour exercer dans le milieu de la formation professionnelle."
+  //   },
+  //   {
+  //     "key": 222222,
+  //     "imgRight": true,
+  //     "image": image_2,
+  //     "alt": "image of a bride",
+  //     "content": "La société C.F.R - Centre de Formation et Recrutement a été créée en  2016 et détient l’autorisation préfectorale pour exercer dans le milieu de la formation professionnelle."
+  //   },
+  //   {
+  //     "key": 3333,
+  //     "imgRight": false,
+  //     "image": image_1,
+  //     "alt": "image of a bride",
+  //     "content": "La société C.F.R - Centre de Formation et Recrutement a été créée en  2016 et détient l’autorisation préfectorale pour exercer dans le milieu de la formation professionnelle."
+  //   }
+  // ];
+
+  return (
+    <div className="home">
+      <Bandeau />
+      {/* <AccueilServices /> */}
+      <ul className="accueil-cards">
+        {
+          cards_data.map((card_data) => (
+            <li key={card_data.key}>
+              <PhotoTextLR props={card_data} />
+            </li>
+          ))
+        }
+      </ul>
+      <FeedbackBox />
+    </div>
+  );
 }
- 
+
 export default Home;
