5bd3e00b643429f9f64104cdedee3c3faa934113
Lem0nRavioli <hirona_mj@hotmail.fr>
2022-02-02 07:03:53 +0100

    carrousel moche pas responsive

--- HpFront/src/components/FeedbackBox/FeedbackBox.js
+++ HpFront/src/components/FeedbackBox/FeedbackBox.js
@@ -1,7 +1,9 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "./FeedbackBox.css";
 import React from "react";
 import Slider from "react-slick";
+import feedbacks from "./feedbacks";
 
 
 
@@ -10,49 +12,26 @@ const FeedbackBox = () => {
         dots: true,
         infinite: true,
         speed: 500,
-        slidesToShow: 3,
+        slidesToShow: 5,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 4000,
     };
 
-    const feedbacks = [
-        {
-            "text": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum, eos.",
-            "author": "Bob"
-        },
-        {
-            "text": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum, eos.",
-            "author": "Bob2"
-        },
-        {
-            "text": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum, eos.",
-            "author": "Bob3"
-        },
-        {
-            "text": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum, eos.",
-            "author": "Bob"
-        },
-        {
-            "text": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum, eos.",
-            "author": "Bob2"
-        },
-        {
-            "text": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum, eos.",
-            "author": "Bob3"
-        }
-    ]
+
     return (
-        <Slider {...settings}>
-            {
-                feedbacks.map((feedback) => (
-                    <div className="box" key={feedback.author}>
-                        <p>{feedback.text}</p>
-                        <h4>{feedback.author}</h4>
-                    </div>
-                ))
-            }
-        </Slider>
+        <div className="feedbackbox">
+            <Slider {...settings}>
+                {
+                    feedbacks.map((feedback) => (
+                        <div className="box" key={feedback.reviewer_id}>
+                            <p>{feedback.text}</p>
+                            <h4>{feedback.reviewer}</h4>
+                        </div>
+                    ))
+                }
+            </Slider>
+        </div>
     );
 }
 
