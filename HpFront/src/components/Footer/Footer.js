616cd0e0472c0561ad027aa8cbfb5c07750e2447
ImNotAOwl <siegel.matthieu@ymail.com>
2022-02-02 12:25:50 +0100

    :white_check_mark: footer OK

--- HpFront/src/components/Footer/Footer.js
+++ HpFront/src/components/Footer/Footer.js
@@ -6,59 +6,70 @@ import instagram from "../../assets/social/instagram.svg";
 import mail from "../../assets/social/mail.svg";
 
 import Adresse from "../Adresse/Adresse";
+import Horaires from "../Horaires/Horaires";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
 
-    const horaireSemaine = [
-        { "jour": "Lundi","heure": "Fermé"},
-        { "jour": "Mardi", "heure": "9h - 19h"},
-        { "jour": "Mercredi", "heure": "9h - 19h"},
-        { "jour": "Jeudi", "heure": "9h - 19h"},
-        { "jour": "Vendredi", "heure": "9h - 19h"},
-        { "jour": "Samedi", "heure": "9h - 19h"},
-        { "jour": "Dimanche", "heure": "Fermé"},
-      ];
-
-      const imagesSocial = [
-          {"src": facebook, "alt": "Logo Facebook", "adresse": "https://www.facebook.com/Hair-Prestige-1532117990421666/"},
-          {"src": twitter, "alt": "Logo Twitter", "adresse": "https://twitter.com/Prestige__Hair"},
-          {"src": instagram, "alt": "Logo Instagram", "adresse": "https://www.instagram.com/hair_prestige_/"},
-          {"src": mail, "alt": "Logo Mail", "adresse": "mailto:hairprestige@gmail.com"},
-      ];
-
+  const imagesSocial = [
+    {
+      src: facebook,
+      alt: "Logo Facebook",
+      adresse: "https://www.facebook.com/Hair-Prestige-1532117990421666/",
+    },
+    {
+      src: twitter,
+      alt: "Logo Twitter",
+      adresse: "https://twitter.com/Prestige__Hair",
+    },
+    {
+      src: instagram,
+      alt: "Logo Instagram",
+      adresse: "https://www.instagram.com/hair_prestige_/",
+    },
+    { src: mail, alt: "Logo Mail", adresse: "mailto:hairprestige@gmail.com" },
+  ];
 
   return (
     <div className="footer">
 
       <div className="footer_up">
-          <div className="social">
-            <h3>Suivez-nous</h3>
-            <div className="logos_social">
-                {
-                    imagesSocial.map((image, index) => {
-                        return (
-                            <a href={image.adresse} target="_blank" rel="noreferrer"><img key={`social-${index}`} src={image.src} alt={image.alt} /></a>
-                        );
-                    })
-                }
-            </div>
-          
-          </div>
 
-          <div className="coordonnees">
-            <Adresse/>
+        <div className="social">
+          <h3>Suivez-nous</h3>
+          <div className="logos_social">
+            {imagesSocial.map((image, index) => {
+              return (
+                <a href={image.adresse} target="_blank" rel="noreferrer">
+                  <img
+                    key={`social-${index}`}
+                    src={image.src}
+                    alt={image.alt}
+                  />
+                </a>
+              );
+            })}
           </div>
+        </div>
 
-          <div className="horaires">
-          </div>
+        <div className="coordonnees">
+          <Adresse />
+        </div>
+
+        <div className="horaires">
+          <Horaires />
+        </div>
       </div>
 
       <div className="footer_down">
-            <hr />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem magnam iusto alias mollitia quia iure odit quasi. Obcaecati cumque est ab consectetur nostrum voluptas facilis quis illum, veniam nemo harum.</p>
+        
+        <hr />
+        <p><Link to="/cgv">Mentions Légales</Link></p>
+        <p>
+        Design with ♥ by Nordine KHOUDOU, Guillaume SAULNIER & Matthieu SIEGEL — © 2022 — All Rights Reserved
+        </p>
+
       </div>
-
-
     </div>
   );
 };
