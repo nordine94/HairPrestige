6f9c7f8bc4ff4194de3380bb72afbf9c14c37942
Khoudou Nordine <nordine-khoudou@hotmail.fr>
2022-02-03 17:01:52 +0100

    modif bandeau

--- HpFront/src/components/Bandeau/Bandeau.js
+++ HpFront/src/components/Bandeau/Bandeau.js
@@ -8,7 +8,7 @@ const Bandeau = () => {
             <div className="left">
                 <h1>Hair Prestige</h1>
                 <h3>Votre salon de coiffure situé à 
-                    Choisy le Roi vous accueille dans un cadre chaleureux et
+                    Choisy le Roi <br/> vous accueille dans un cadre chaleureux <br/>et
                     convivial.</h3>
                 {/* <button>Reserver</button> */}
                 {/* <div className="right"></div> */}
