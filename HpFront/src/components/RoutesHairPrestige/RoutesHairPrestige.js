269e120cdf56f7c7c887aa90c419560c989204e5
Lem0nRavioli <hirona_mj@hotmail.fr>
2022-02-03 21:15:26 +0100

    ajout gmap_cheap

--- HpFront/src/components/RoutesHairPrestige/RoutesHairPrestige.js
+++ HpFront/src/components/RoutesHairPrestige/RoutesHairPrestige.js
@@ -4,15 +4,17 @@ import Conseils from '../../pages/Conseils/Conseils';
 import Cgv from '../../components/Cgv/Cgv';
 import Photos from "../../pages/Photos/Photos";
 import Prestations from '../../pages/Prestations/Prestations';
+import Contact from '../../pages/Contact/Contact';
 
 const RoutesHairPrestige = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/conseils" element={<Conseils />} />
-            <Route path="/cgv" element={<Cgv />} />
-            <Route path="/photos" element={<Photos />} />
+            <Route path="/contact" element={<Contact />} />
             <Route path="/prestations" element={<Prestations/>} />
+            <Route path="/photos" element={<Photos />} />
+            <Route path="/cgv" element={<Cgv />} />
         </Routes>
     );
 }
