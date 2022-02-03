ec34c9a267e8b4ebb3329ab2f82c815fa140cdec
Lem0nRavioli <hirona_mj@hotmail.fr>
2022-02-03 17:25:46 +0100

    update typo horaire.js

--- HpFront/src/components/Horaires/Horaires.js
+++ HpFront/src/components/Horaires/Horaires.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./Horaires.css";
 
 const Horaires = () => {
@@ -32,38 +31,3 @@ const Horaires = () => {
 };
 
 export default Horaires;
-=======
-import "./Horaires.css";
-
-const Horaires = () => {
-  const horaireSemaine = [
-    { jour: "Lundi", heure: "Fermé" },
-    { jour: "Mardi", heure: "9h - 19h" },
-    { jour: "Mercredi", heure: "9h - 19h" },
-    { jour: "Jeudi", heure: "9h - 19h" },
-    { jour: "Vendredi", heure: "9h - 19h" },
-    { jour: "Samedi", heure: "9h - 19h" },
-    { jour: "Dimanche", heure: "Fermé" },
-  ];
-
-  return (
-    <div className="horaires_salon">
-      <h3>Horaires du salon</h3>
-      <table>
-        <tbody>
-          {horaireSemaine.map((horaireJour, index) => {
-            return (
-              <tr>
-                <td> {horaireJour.jour} </td>
-                <td> {horaireJour.heure} </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
-};
-
-export default Horaires;
->>>>>>> 9f0a2f93d9da4bc7e1e92b082295280e5c2f4a56
