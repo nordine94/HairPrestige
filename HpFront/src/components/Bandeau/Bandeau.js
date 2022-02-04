28d5be4a19b04c3c3bcff9919fd54b9f4ee07d99
Khoudou Nordine <nordine-khoudou@hotmail.fr>
2022-02-04 21:38:34 +0100

    props texte et titre

--- HpFront/src/components/Bandeau/Bandeau.js
+++ HpFront/src/components/Bandeau/Bandeau.js
@@ -2,21 +2,21 @@ import "./Bandeau.css";
 
 
 
-const Bandeau = ({image}) => {
-    return ( 
-        <div className="bandeau" style={{backgroundImage:`url(${image})` }}>
-            <div className="left">
-                <h1>Hair Prestige</h1>
-                
-                <h3>Votre salon de coiffure situé à 
-                    Choisy le Roi <br/> vous accueille dans un cadre chaleureux <br/>et
-                    convivial.</h3>
-                <button>Reserver</button>
-                {/* <div className="right"></div> */}
-            </div>
-               
+const Bandeau = ({image, titre, texte}) => {
+    return (
+      <div className="bandeau" style={{ backgroundImage: `url(${image})` }}>
+        <div className="left">
+          <h1 >Hair Prestige {titre}</h1>
+          <h3>
+            Votre salon de coiffure situé à Choisy le Roi <br /> vous accueille
+            dans un cadre chaleureux <br />
+            et convivial.{texte}
+          </h3>
+          <button>Reserver</button>
+          {/* <div className="right"></div> */}
         </div>
-     );
+      </div>
+    );
 }
  
 
--- HpFront/src/pages/Home/Home.js
+++ HpFront/src/pages/Home/Home.js
@@ -1,5 +1,7 @@
 import Bandeau from '../../components/Bandeau/Bandeau';
-import bandeauImage from "../../assets/img/bride.jpg";
+import bandeauImage from "../../assets/img/bandeau.jpg";
+import h1Bandeau from "../../components/Bandeau/Bandeau";
+import h3Bandeau from "../../components/Bandeau/Bandeau";
 import FeedbackBox from '../../components/FeedbackBox/FeedbackBox';
 import './Home.css';
 
@@ -9,7 +11,7 @@ import cards_data from './home_cards_data';
 const Home = () => {
   return (
     <div className="home">
-      <Bandeau image={bandeauImage} />
+      <Bandeau image={bandeauImage} titre={h1Bandeau} texte={h3Bandeau}/>
       <ul className="accueil-cards">
         {
           cards_data.map((card_data) => (
