bc9cbe38556edabc4b325356c65aea5bca3bfbbf
Khoudou Nordine <nordine-khoudou@hotmail.fr>
2022-02-07 12:41:14 +0100

    props bandeau

--- HpFront/src/components/Bandeau/Bandeau.js
+++ HpFront/src/components/Bandeau/Bandeau.js
@@ -3,20 +3,25 @@ import "./Bandeau.css";
 
 
 const Bandeau = ({image, titre, texte}) => {
-    return (
-      <div className="bandeau" style={{ backgroundImage: `url(${image})` }}>
-        <div className="left">
-          <h1 >Hair Prestige {titre}</h1>
-          <h3>
-            Votre salon de coiffure situé à Choisy le Roi <br /> vous accueille
-            dans un cadre chaleureux <br />
-            et convivial.{texte}
-          </h3>
-          <button>Reserver</button>
-          {/* <div className="right"></div> */}
-        </div>
+
+ 
+  return (
+    <div className="bandeau" style={{ backgroundImage: `url(${image})` }}>
+      <div className="left">
+        <h1>{titre}</h1>
+        <h3>
+          {texte.map((texte, index) => {
+            console.log(texte);
+            return (
+              <>{texte} <br/></>
+            )
+            })}
+        </h3>
+        <button>Reserver</button>
+        {/* <div className="right"></div> */}
       </div>
-    );
+    </div>
+  );
 }
  
 
--- HpFront/src/pages/Home/Home.js
+++ HpFront/src/pages/Home/Home.js
@@ -1,17 +1,22 @@
 import Bandeau from '../../components/Bandeau/Bandeau';
 import bandeauImage from "../../assets/img/bandeau.jpg";
-import h1Bandeau from "../../components/Bandeau/Bandeau";
-import h3Bandeau from "../../components/Bandeau/Bandeau";
 import FeedbackBox from '../../components/FeedbackBox/FeedbackBox';
 import './Home.css';
-
 import PhotoTextLR from '../../components/PhotoTextLR/PhotoTextLR';
 import cards_data from './home_cards_data';
 
 const Home = () => {
+
+  const contenuBandeau = {
+    titre:"Hair Prestige",
+    texte : [
+     "Votre salon de coiffure situé à Choisy le Roi",
+     "vous accueille dans un cadre chaleureux et convivial.",
+    ]}
+
   return (
     <div className="home">
-      <Bandeau image={bandeauImage} titre={h1Bandeau} texte={h3Bandeau}/>
+      <Bandeau image={bandeauImage} titre={contenuBandeau.titre} texte={contenuBandeau.texte}/>
       <ul className="accueil-cards">
         {
           cards_data.map((card_data) => (
