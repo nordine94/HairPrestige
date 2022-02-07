9984f0e51849cd0b7293ab91a62b09e1fa01105d
Lem0nRavioli <hirona_mj@hotmail.fr>
2022-02-08 00:50:09 +0100

    added moche css

--- HpFront/src/pages/Contact/Contact.js
+++ HpFront/src/pages/Contact/Contact.js
@@ -11,6 +11,7 @@ const Contact = () => {
         email: '',
     });
 
+    // on peux éventuellement faire une thank you page
     const genericFormData = {
         "toSend": toSend,
         "setToSend": setToSend,
@@ -19,12 +20,14 @@ const Contact = () => {
         "redirect": 'http://localhost:3000/photos'
     }
 
-    return ( 
+    return (
         <div className="contact">
+            <h1>Nous contacter</h1>
+            <hr/>
             <GenericForm props={genericFormData} />
             <GmapCheap image={gmap_image} link="https://goo.gl/maps/zbSxBn2TP3t661XZA" />
         </div>
-     );
+    );
 }
- 
+
 export default Contact;
