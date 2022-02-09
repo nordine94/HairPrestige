1c265f20d3ce35363faede56fa111ef0e4d9cdb0
Khoudou Nordine <nordine-khoudou@hotmail.fr>
2022-02-09 18:16:46 +0100

    merge

--- HpFront/src/components/Bandeau/Bandeau.js
+++ HpFront/src/components/Bandeau/Bandeau.js
@@ -1,55 +1,22 @@
-<<<<<<< HEAD
 import "./Bandeau.css";
 
-
-
-const Bandeau = () => {
-
-    const bob = [{
-      "image": "../../assets/img/bandeau.jpg",
-      "titre": "Hair Prestige",
-      "texte":"Votre salon de coiffure situé à Choisy le Roi vous accueille dans un cadre chaleureux et convivial.",
-    }];
-
-    return (
-      bob.map((bob, index) => (
-       <div className="bandeau" style={{ backgroundImage: `url(${bob.image})` }} key={`link-${index}`}>
-        <div className="left">
-          <h1 >{bob.titre}</h1>
-          <h3>{bob.texte}</h3>
-          {/* <button>Reserver</button> */}
-          {/* <div className="right"></div> */}
-       ) </div>
-      </div>
-      ))
-    
-    )
-  }
- 
-
-=======
-import "./Bandeau.css";
-
-
-const Bandeau = ({image, titre, texte}) => {
-
+const Bandeau = ({ image, titre, texte }) => {
   return (
     <div className="bandeau" style={{ backgroundImage: `url(${image})` }}>
       <div className="left">
-        <h1>{titre}</h1>        
-          {texte.map((texte, index) => {          
-              return (
-              <h3 key={`${index}`}>
-                <>
-                  {texte} <br />
-                </>
-              </h3>
-              );           
-          })}             
+        <h1>{titre}</h1>
+        {texte.map((texte, index) => {
+          return (
+            <h3 key={`${index}`}>
+              <>
+                {texte} <br />
+              </>
+            </h3>
+          );
+        })}
       </div>
     </div>
   );
-}
- 
->>>>>>> develop
+};
+
 export default Bandeau;
--- HpFront/src/pages/Home/Home.js
+++ HpFront/src/pages/Home/Home.js
@@ -1,43 +1,37 @@
-import Bandeau from '../../components/Bandeau/Bandeau';
-<<<<<<< HEAD
-=======
+import Bandeau from "../../components/Bandeau/Bandeau";
 import bandeauImage from "../../assets/img/photo_un.jpg";
->>>>>>> develop
-import FeedbackBox from '../../components/FeedbackBox/FeedbackBox';
-import './Home.css';
-import PhotoTextLR from '../../components/PhotoTextLR/PhotoTextLR';
-import cards_data from './home_cards_data';
+import FeedbackBox from "../../components/FeedbackBox/FeedbackBox";
+import "./Home.css";
+import PhotoTextLR from "../../components/PhotoTextLR/PhotoTextLR";
+import cards_data from "./home_cards_data";
 
 const Home = () => {
-
   const contenuBandeau = {
-    titre:"Hair Prestige",
-    texte : [
-     "Votre salon de coiffure situé à Choisy le Roi",
-     "vous accueille dans un cadre chaleureux et convivial.",
-    ]}
+    titre: "Hair Prestige",
+    texte: [
+      "Votre salon de coiffure situé à Choisy le Roi",
+      "vous accueille dans un cadre chaleureux et convivial.",
+    ],
+  };
 
   return (
     <div className="home">
-<<<<<<< HEAD
-      <Bandeau />
-=======
-      <Bandeau image={bandeauImage} titre={contenuBandeau.titre} texte={contenuBandeau.texte}/>
->>>>>>> develop
+      <Bandeau
+        image={bandeauImage}
+        titre={contenuBandeau.titre}
+        texte={contenuBandeau.texte}
+      />
       <ul className="accueil-cards">
-        {
-          cards_data.map((card_data, index) => (
-            <li key={`card_home-${index}`}>
-              <PhotoTextLR props={card_data} />
-              <hr />
-            </li>
-            
-          ))
-        }
+        {cards_data.map((card_data, index) => (
+          <li key={`card_home-${index}`}>
+            <PhotoTextLR props={card_data} />
+            <hr />
+          </li>
+        ))}
       </ul>
       <FeedbackBox />
     </div>
   );
-}
+};
 
 export default Home;
