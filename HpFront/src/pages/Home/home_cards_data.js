f7ffeb56867541211697a3765d65b11ab783681e
ImNotAOwl <siegel.matthieu@ymail.com>
2022-02-09 18:15:23 +0100

    changes photoTextLR + content wordpress

--- HpFront/src/pages/Home/home_cards_data.js
+++ HpFront/src/pages/Home/home_cards_data.js
@@ -1,8 +1,6 @@
-import image_kid from '../../assets/img/haircut-kid.jpg';
-import image_chignon from '../../assets/img/pic_chignon.jpg';
-import image_barbe from '../../assets/img/barba.webp';
-import Adresse from '../../components/Adresse/Adresse';
-import Horaires from '../../components/Horaires/Horaires';
+import image_kid from '../../assets/img/washmahair.jpg';
+import image_bride from '../../assets/img/bride.jpg';
+import image_barbe from '../../assets/img/friser_salon.webp';
 
 export const home_cards_data = [
     {
@@ -10,18 +8,17 @@ export const home_cards_data = [
         "image": image_kid,
         "alt": "image of a kid getting haircut",
         "content": [
-            "Situé à Choisy le Roi, le salon Hair Prestige vous accueille du mardi au samedi dans un cadre convivial et agréable.",
-            "C'est un salon de coiffure aux allures rock'n roll qui envoie bouler les codes. Que l'on soit un homme ou une femme, ici le tarif est le même et on y vient pour s'offrir une coupe dans une ambiance jeune et cosy, une décoration vintage avec une playlist bien pensée.",
-            "Ici, vous y serez aussi à l'aise que chez vos potes. Rien n'est normé, même la déco se prive de symétrie et d'homogénéité, rendant ainsi le lieu plus personnel. Ce côté rock résonne jusque dans les oreilles ! Voilà un coiffeur qui va vous faire rocker !",
-            "Mesdames, Messieurs, changez de coupe ou de couleur selon vos envies chez Hair Prestige... à Choisy le Roi !"
+            <h2>Vous recherchez un salon proposant la coloration végétale et des Shampoings Bio en Ile de France ?</h2>,
+            "A chaque coloration, on prend soin de vos cheveux et de votre cuir chevelu. Une coloration naturelle pour une meilleure santé des cheveux renforcés et même une chute de cheveu ralentie. Lavez vos cheveux naturellement, sans détergent ; nos agents lavants dérivés d’acides aminés naturels conservent l’hydratation naturelle de la peau."
         ]
     },
     {
         "imgRight": true,
-        "image": image_chignon,
+        "image": image_bride,
         "alt": "image femme chignon",
         "content": [
-            <Horaires />
+            <h2>Mariages et Soirée</h2>,
+            "Nous vous proposons des forfaits pour vos coiffures de mariage et de soirée."
         ]
     },
     {
@@ -29,8 +26,8 @@ export const home_cards_data = [
         "image": image_barbe,
         "alt": "image homme barbe",
         "content": [
-            "Ou sinon un numéro de tel",
-            <Adresse />
+            <h2>Lissage</h2>,
+            "Qui n’a jamais rêvé de cheveux lisses, faciles à coiffer et qui ne gonflent pas au contact de l’humidité ? Nous vous trouvons la solution adaptée à vos cheveux."
         ]
     }
 ];
