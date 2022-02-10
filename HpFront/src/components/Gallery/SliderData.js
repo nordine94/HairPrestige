860ee88b6e47121f1999e30cef3594ecc33e5b19
Khoudou Nordine <nordine-khoudou@hotmail.fr>
2022-02-10 19:09:03 +0100

    gallery avec responsive degeu

--- HpFront/src/components/Gallery/SliderData.js
+++ HpFront/src/components/Gallery/SliderData.js
@@ -1,8 +1,8 @@
 import salon1 from "../../assets/salon/salon1.JPG";
-import salon2 from "../../assets/salon/salon1.JPG";
-import salon3 from "../../assets/salon/salon1.JPG";
-import salon4 from "../../assets/salon/salon1.JPG";
-import salon5 from "../../assets/salon/salon1.JPG";
+import salon2 from "../../assets/salon/salon1.jpeg";
+import salon3 from "../../assets/salon/salon3.JPG";
+import salon4 from "../../assets/salon/salon4.JPG";
+import salon5 from "../../assets/salon/salon5.JPG";
 
 export const SliderData = [
   {
