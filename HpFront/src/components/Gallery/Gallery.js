059ec3b905bb565c3456816141a7949693c19451
Lem0nRavioli <hirona_mj@hotmail.fr>
2022-02-10 23:47:24 +0100

    fixed warnings for push

--- HpFront/src/components/Gallery/Gallery.js
+++ HpFront/src/components/Gallery/Gallery.js
@@ -31,7 +31,7 @@ const Gallery = ({ slides }) => {
             key={index}
           >
             {index === current && (
-              <img src={slide.image} alt="travel image" className="image" />
+              <img src={slide.image} alt="travel" className="image" />
             )}
           </div>
         );
--- HpFront/src/components/Gmap_cheap/GmapCheap.js
+++ HpFront/src/components/Gmap_cheap/GmapCheap.js
@@ -3,8 +3,8 @@ import './GmapCheap.css'
 const GmapCheap = ({ image, link }) => {
     return (
         <div className="gmapcheap">
-            <a href={link} target="_blank">
-                <img src={image} alt="google map image" />
+            <a href={link} target="_blank" rel="noreferrer">
+                <img src={image} alt="google map" />
             </a>
         </div>
     );
